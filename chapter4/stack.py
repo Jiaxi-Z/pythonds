41dfbb998bff1c45ca94e712fa7d2d550fb1021e
Jiaxi-Z <zhaojiaxi1996@163.com>
2021-02-03 16:38:14 +0800

    master

---
+++ chapter4/stack.py
@@ -0,0 +1,26 @@
+# Implementing a Stack class using Python lists (stack_1ac)
+class Stack:
+    def __init__(self):
+        self.items = []
+
+    def isEmpty(self):
+        return self.items == []
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        return self.items.pop()
+
+    def peek(self):
+        return self.items[len(self.items)-1]
+
+    def size(self):
+        return len(self.items)
+
+
+s = Stack()
+print(s.isEmpty())
+s.push(3)
+print(s.items)
+print(s.isEmpty())
